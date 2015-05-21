3ee55f94a2d4e40fe47251f09529f3a4b9d72c76
Daniel Diaz <dddiaz@uci.edu>
2015-05-20 20:55:06 -0700

    simple form working

--- website/search/views.py
+++ website/search/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import RequestContext, loader
 
 # Create your views here.
 def index(request):
-	#return render(request,'search/index.html',{})
-	#return HttpResponse('Test')
-	template = loader.get_template('search/index.html')
-	context = RequestContext(request, {})
-	return HttpResponse(template.render(context))
+    return render (request,'search/index.html')
+
+#Based on this example
+#http://www.djangobook.com/en/2.0/chapter07.html
+def search(request):
+    if 'q' in request.GET:
+        message = 'You searched for: %r' % request.GET['q']
+    else:
+        message = 'You submitted an empty search.'
+    return HttpResponse(message)
+
+# from mysite.books.models import Book
+#
+# def search(request):
+#     if 'q' in request.GET and request.GET['q']:
+#         q = request.GET['q']
+#         books = Book.objects.filter(title__icontains=q)
+#         return render(request, 'search_results.html',
+#             {'books': books, 'query': q})
+#     else:
+#         return HttpResponse('Please submit a search term.')
--- website/website/urls.py
+++ website/website/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
 	url(r'^search/', include('search.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    #url(r'^&', include('search.urls')),
 ]
