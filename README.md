62c12cd643d9235c07258baecde85a171253a13c
Daniel Diaz <dddiaz@uci.edu>
2015-06-02 23:34:56 -0700

    working search engine with 2 gram support

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 Python Search Engine written with Django
 
 To get this project to work you need the necesarry pickle files 
-i have put them in the root
+
  also dont forget to change the path for those files in pickleindex.py
+
 
  lastly, when starting up the project make sure to navigate to /search
  
--- website/search/pickleindex.py
+++ website/search/pickleindex.py
@@ -1,9 +1,11 @@
 __author__ = 'danieldiaz'
 import pickle
+import operator
 
 class SearchResults:
     def __init__(self):
         self.results = {}
+        self.maxResults = 10
         self.search_phrase = ''
         self.term1 = None
         self.term2 = None
@@ -13,6 +15,13 @@ class SearchResults:
         self.term2termid = pickle.load( open( "/Users/danieldiaz/Desktop/PickleFiles/term2termID.pk", "rb" ) )
         self.term2tfidf = pickle.load(open("/Users/danieldiaz/Desktop/PickleFiles/term2TFIDF.pk","rb"))
         self.docid2url = pickle.load(open("/Users/danieldiaz/Desktop/PickleFiles/docID2URL.pk","rb"))
+        self.websiteresultsDict = None
+        self.sorted_docs1 = None
+        self.websiteresultsDict1 = None
+        self.sorted_docs2 = None
+        self.websiteresultsDict2 = None
+        self.termIDExists1 = True
+        self.termIDExists2 = True
 
     def calcResults(self,search_phrase):
         self.search_phrase = search_phrase
@@ -35,70 +44,114 @@ class SearchResults:
 
     def calc1gram(self):
         self.termid1 = self.term_id_from_term(self.term1)
-        if (self.termid1 is not None):
-            self.finddocs(self.termid1)
+        if (self.termid1 != ''):
+            (self.docs, self.sorted_docs, self.websiteresultsDict1) = self.finddocs(self.termid1)
+            self.websiteresultsDict = self.websiteresultsDict1
         #check result is not none
+        else:
+            self.websiteresultsDict = {}
+            self.websiteresultsDict[0] = "No Results for that Query"
+
         return
 
     def calc2gram(self):
+        self.termid1 = self.term_id_from_term(self.term1)
+        self.termid2 = self.term_id_from_term(self.term2)
+        #results for first word
+        if (self.termid1 != ''):
+            (self.docs1, self.sorted_docs1, self.websiteresultsDict1) = self.finddocs(self.termid1)
+        else:
+            self.websiteresultsDict1 = None
+        #resultd for second word
+        if (self.termid2 != ''):
+            (self.docs2, self.sorted_docs2, self.websiteresultsDict1) = self.finddocs(self.termid2)
+        else:
+            self.websiteresultsDict2 = None
+        #Zipper results
+        (self.docs, self.sorted_docs, self.websiteresultsDict) = self.zipperresults(self.docs1,self.docs2)
+        #will write results in websiteresultsDict
         return
 
     def constructResults(self):
         self.results['search_phrase'] = self.search_phrase
         self.results['term_id1'] = self.termid1
+        self.results['websites'] = self.websiteresultsDict
         return self.results
 
     def term_id_from_term(self,term):
         try:
-            term = term.lower();
-            result = str(self.term2termid[term]);
+            term = term.lower()
+            result = str(self.term2termid[term])
         except:
             result = None
             #result = "term not found"
         return result
 
     def finddocs(self,termid):
-        docs = self.term2tfidf[termid]
-        firstdoc = docs[1]
-        firstpage = self.docid2url[firstdoc]
+        docs = self.term2tfidf[int(termid)]
+        #print(docs)
+        sorted_docs = sorted(docs.items(), key=operator.itemgetter(1), reverse=True)
+        #print("Sorted Docs")
+            #print(sorted_docs)
 
+            #firstpage = self.docid2url[firstdoc]
+        websiteresultsDict = {}
 
+        count = 0
+        for tuple in sorted_docs:
+            if (count < self.maxResults):
+                websiteresultsDict[count] = self.docid2url[tuple[0]]
+            else:
+                break
+            count += 1
 
+        return docs, sorted_docs, websiteresultsDict
 
+    def zipperresults(self,x,y):
+        #base case
+        #if both none
+        if (x is None and y is None):
+            self.websiteresultsDict = {}
+            self.websiteresultsDict[0] = "No Results for that Query"
+            return
+        #if x is none
+        if (x is None):
+            self.websiteresultsDict = self.websiteresultsDict2
+            return
+        #if y is none
+        if (y is None):
+            self.websiteresultsDict = self.websiteresultsDict1
+            return
 
+        #zipper results
+        zipperDict = {} #siteid, score
+        count = 0
+        for k,v in x.items():
+            #k is doc id
+            #v is score
+            ######ONLY CARE ABOUT TOP 10 RESULTS
+            if(count < self.maxResults):
+                #check if doc id in other doc
+                try:
+                    yscore = y[int(k)]
+                except:
+                    yscore = 0
+                zipperDict[k] = (yscore + v)/2
+            else:
+                break
+            count += 1
 
+        #reorder doc
+        sorted_docs = sorted(zipperDict.items(), key=operator.itemgetter(1), reverse=True)
 
+        count = 0
+        websiteresultsDict = {}
+        for tuple in sorted_docs:
+            if (count < self.maxResults):
+                websiteresultsDict[count] = self.docid2url[tuple[0]]
+            else:
+                break
+            count += 1
 
+        return zipperDict, sorted_docs, websiteresultsDict
 
-
-
-# def return_query_results(search_phrase):
-#     #init results dict
-#     results = {};
-#     term2termid = pickle.load( open( "/Users/danieldiaz/Desktop/PickleFiles/term2termID.pk", "rb" ) )
-#     term2tfidf = pickle.load(open("/Users/danieldiaz/Desktop/PickleFiles/term2TFIDF.pk","rb"))
-#     docid2url = pickle.load(open("/Users/danieldiaz/Desktop/PickleFiles/docID2URL.pk","rb"))
-#
-#     #figure out if 2 gram or single word
-#     terms = search_phrase.split()
-#     if (len(terms) == 1):
-#         #do normal logic
-#         z=1
-#         #results = onegram(results,terms,)
-#     else:
-#         #its a 2gram...hopefully
-#         z=1
-#
-#     results['search_phrase'] = search_phrase
-#     results['term_id'] = term_id_from_term(search_phrase,term2termid)
-#
-#     return results
-#
-# def term_id_from_term(term,term2termid):
-#     try:
-#         term = term.lower();
-#         #word_hash = pickle.load( open( "/Users/danieldiaz/Desktop/PickleFiles/term2termID.pk", "rb" ) )
-#         result = str(term2termid[term]);
-#     except:
-#         result = "\nword not found\n"
-#     return result
